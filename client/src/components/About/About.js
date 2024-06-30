5dc7d5a8f9db90cef450ec8960c2c4318cb6c341
Haile <haile4cmd@gmail.com>
2024-06-30 20:00:24 +0300

    build: add about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='about'>
+      <h1>How To Play</h1>
+      <div className='aboutinfo'>
+        <p>Guess the Wordle in 6 tries.</p>
+        <p>- Each guess must be a valid n-letter word.</p>
+        <p>- The color of the tiles will change to show how close your guess was to the word.</p>
+        <p>- green (correct), yellow (misplaced) or red (incorrect)</p>
+        <p>- User can decide how many letters the word should have and whether it can contain repeated letters</p>
+      </div>
     </div>
   )
 }
